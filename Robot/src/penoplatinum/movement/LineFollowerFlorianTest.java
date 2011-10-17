5db1771038f9dbf90731d06707b4864a43cdd72c
Florian <mister_florian@hotmail.com>
2011-10-17 18:29:28 +0200

    hahahah lul
    Signed-off-by: Florian <mister_florian@hotmail.com>

---
+++ Robot/src/penoplatinum/movement/LineFollowerFlorianTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PenoPlatinum.movement;
+
+/**
+ *
+ * @author Florian
+ */
+public class LineFollowerFlorianTest {
+    static LineFollowerFlorian abc = new LineFollowerFlorian();
+    public static void main(String[]args){
+        abc.ActionLineFollower();
+    }
+    
+}
